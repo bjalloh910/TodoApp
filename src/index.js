15c5db8c32c4355ed2354bad625a315944a97d6f
Bintu <bjalloh@smith.edu>
2025-01-05 12:33:28 -0600

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // index.js
-import React from 'react';
-
-import { UncontrolledExample } from './slideComponent.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createRoot } from 'react-dom/client';
+//import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import { createRoot } from 'react-dom/client';
 import "./styles.css"; // importing the css file into the js file
 //import cakeImage from "./assetspic/choc_cake.png";
 import { greeting } from "./greeting.js";
@@ -18,7 +16,7 @@ console.log("Hello Bintu");
 
 
 //ReactDOM.render is no longer supported in React 18. Use createRoot instead.
-const container = document.getElementById('slide'); // Make sure this matches your HTML
-const root = createRoot(container);
-root.render(<UncontrolledExample />);
+//const container = document.getElementById('slide'); // Make sure this matches your HTML
+//const root = createRoot(container);
+//root.render(<UncontrolledExample />);
 
